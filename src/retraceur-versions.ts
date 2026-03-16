a5e5f6584cce8605ec25cfe82de2238b34202d0d
imath <math.viet@gmail.com>
2026-03-16 22:10:52 +0100

    Include Retraceur 3.1.0 to supported versions
    Fixes #5

--- src/retraceur-versions.ts
+++ src/retraceur-versions.ts
@@ -21,8 +21,13 @@ export const RETRACEUR_VERSIONS: Record<string, RetraceurVersion> = {
 		releaseDate: 'rolling'
 	},
 	'latest': {
-		tag: '3.0.0',
+		tag: '3.1.0',
 		phpVersion: '8.3',
+		releaseDate: 'rolling'
+	},
+	'3.1.0': {
+		tag: '3.1.0',
+		phpVersion: '8.2',
 		releaseDate: 'rolling'
 	},
 	'3.0.0': {
